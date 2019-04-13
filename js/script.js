bd47dc58c4e9004ee03273634c7abc6c389cc986
Xurxe <xurxe.garcia@integrify.io>
2019-04-13 22:20:16 +0300

    Made sidebar untuck on horizontal resize

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const main = document.querySelector('main');
 const navLogoDiv = document.querySelector('.nav-logo-div');
 const navDivs = document.querySelectorAll('.nav-div');
 
+let hamburgerTucked;
 
 
 function hideNavContent() {
@@ -12,6 +13,7 @@ function hideNavContent() {
     function runEffect() {
         if (opacity < 0) {
             for (let i = 0; i < navDivs.length; i++) {
+                navDivs[i].style.opacity = '0';
                 navDivs[i].style.display = 'none';
             };
 
@@ -19,7 +21,38 @@ function hideNavContent() {
         }
 
         else {
-            opacity -= 0.05;
+            opacity -= 0.03;
+
+            for (let i = 0; i < navDivs.length; i++) {
+                navDivs[i].style.opacity = opacity;
+            };
+        };
+    };
+};
+
+
+
+function showNavContent() {
+    let opacity = 0;
+    for (let i = 0; i < navDivs.length; i++) {
+        navDivs[i].style.opacity = opacity;
+        navDivs[i].style.display = 'block';
+    };
+
+    let effect = setInterval(runEffect, 1);
+
+    function runEffect() {
+        if (opacity > 1) {
+            for (let i = 0; i < navDivs.length; i++) {
+                navDivs[i].style.opacity = 1;
+            };
+
+            clearInterval(effect);
+            return;
+        }
+
+        else {
+            opacity += 0.03;
 
             for (let i = 0; i < navDivs.length; i++) {
                 navDivs[i].style.opacity = opacity;
@@ -33,17 +66,21 @@ function hideNavContent() {
 function tuckHamburger() {
     let navWidth = nav.style.width;
     navWidth = navWidth.substring(0, navWidth.length - 3);
+    navWidth = parseInt(navWidth);
 
     let mainPaddingLeft = main.style.paddingLeft;
     mainPaddingLeft = mainPaddingLeft.substring(0, mainPaddingLeft.length - 3);
+    mainPaddingLeft = parseInt(mainPaddingLeft);
 
     let effect = setInterval(runEffect, 1);
 
     function runEffect() {
-        if (Math.abs(navWidth - 3) < 0.1) {
+        if (navWidth < 3) {
             nav.style.width = '3rem';
             main.style.paddingLeft = '5rem';
+            hamburgerTucked = true;
             clearInterval(effect);
+            return;
         }
 
         else {
@@ -57,18 +94,73 @@ function tuckHamburger() {
 
 
 
+function untuckHamburger() {
+    let navWidth = nav.style.width;
+    navWidth = navWidth.substring(0, navWidth.length - 3);
+    navWidth = parseInt(navWidth);
+
+    let mainPaddingLeft = main.style.paddingLeft;
+    mainPaddingLeft = mainPaddingLeft.substring(0, mainPaddingLeft.length - 3);
+    mainPaddingLeft = parseInt(mainPaddingLeft);
+
+    let effect = setInterval(runEffect, 1);
+
+    function runEffect() {
+        if (navWidth > 10) {
+            nav.style.width = '10rem';
+            main.style.paddingLeft = '12rem';
+            hamburgerTucked = false;
+
+            clearInterval(effect);
+            return;
+        }
+
+        else {
+            navWidth += 0.1;
+            mainPaddingLeft += 0.1;
+            nav.style.width = `${navWidth}rem`;
+            main.style.paddingLeft = `${mainPaddingLeft}rem`;
+        };
+    };
+};
+
+
+
 window.addEventListener('resize', function() {
     if (window.innerWidth > 600) {
-        nav.style.width = `10rem`;
-        main.style.paddingLeft = '12rem';
-        for (let i = 0; i < navDivs.length; i++) {
-            navDivs[i].style.opacity = 1;
-            navDivs[i].style.display = 'block';
+        if (hamburgerTucked === true) {
+            untuckHamburger();
+            setTimeout(showNavContent, 300);
         };
     };
 
     if (window.innerWidth < 600) {
-        hideNavContent();
-        tuckHamburger();
+        if (hamburgerTucked === false) {
+            hideNavContent();
+            setTimeout(tuckHamburger, 100);
+        };
     };
 });
+
+
+function initialize() {
+
+    if (window.innerWidth > 600) {
+        nav.style.width = '10rem';
+        main.style.paddingLeft = '12rem';
+        hamburgerTucked = false;
+    }
+
+    else {
+        nav.style.width = '3rem';
+        main.style.paddingLeft = '5rem';
+        hamburgerTucked = true;
+
+        for (let i = 0; i < navDivs.length; i++) {
+            navDivs[i].style.opacity = '0';
+            navDivs[i].style.display = 'none';
+        };
+    };
+};
+
+initialize();
